55f5ebbe5407f67bec87e85b3a1dd198d4425b36
Sorgfalt <yeongjun1324@gmail.com>
2022-07-29 18:39:53 +0900

    project setting

---
+++ src/main/java/com/firststepapi/FirststepApiApplication.java
@@ -0,0 +1,13 @@
+package com.firststepapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirststepApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirststepApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firststepapi/FirststepApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firststepapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirststepApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
